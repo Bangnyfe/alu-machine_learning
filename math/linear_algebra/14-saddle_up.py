d4c6a099b9426db26fa58559677a6e0493b64323
Bangnyfe <a.daib@alustudent.com>
2026-09-12 12:44:00 -0700

    #

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines a function that performs matrix multiplication."""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication on two NumPy arrays."""
+    return np.matmul(mat1, mat2)
