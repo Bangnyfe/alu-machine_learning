c4709e9b97506c7c409bdd58dffdb87e77dd1391
Bangnyfe <a.daib@alustudent.com>
2026-09-12 12:40:31 -0700

    #

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines a function that transposes a NumPy array."""
+
+
+def np_transpose(matrix):
+    """Returns the transpose of a NumPy array."""
+    return matrix.transpose()
